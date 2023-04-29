b64a86694f013a79031cb1907551cbd262c6d75c
Nezo96 <39273050+Nezo96@users.noreply.github.com>
2023-04-29 20:28:14 +0000

    Added name to state to save string

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: "Radoslav",
+    };
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>My name is Rado</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>My name is {this.state.name}</p>
+          <button>Change name</button>
         </header>
       </div>
     );
