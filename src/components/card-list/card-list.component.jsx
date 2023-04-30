2c52b095cc5e9183ef5c7bb879a29cf40f9109c1
Nezo96 <racradoslav96@gmail.com>
2023-04-30 07:47:44 +0200

    Added card component and moved card to card component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import "./card-list.styles.css";
+import Card from "../card/card.component";
 
 class CardList extends Component {
   render() {
@@ -10,14 +11,20 @@ class CardList extends Component {
         {monsters.map((monster) => {
           const { id, name, email } = monster;
           return (
-            <div className="card-container" key={id}>
-              <img
-                src={`https://robohash.org/${id}?set=set2&size=180x180`}
-                alt={`monster ${name}`}
-              />
-              <h2>{name}</h2>
-              <p>{email}</p>
-            </div>
+            <Card
+              className={"card-container"}
+              id={id}
+              name={name}
+              email={email}
+            />
+            // <div className="card-container" key={id}>
+            //   <img
+            //     src={`https://robohash.org/${id}?set=set2&size=180x180`}
+            //     alt={`monster ${name}`}
+            //   />
+            //   <h2>{name}</h2>
+            //   <p>{email}</p>
+            // </div>
           );
         })}
       </div>
