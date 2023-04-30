4d171221a0c23e09b8f34d675ab0d01a631c6694
Nezo96 <39273050+Nezo96@users.noreply.github.com>
2023-04-30 02:53:09 +0000

    Updated cardlist component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Component } from "react";
 
 import logo from "./logo.svg";
 import "./App.css";
+import CardList from "./components/card-list/card-list.component";
 
 class App extends Component {
   constructor() {
@@ -56,13 +57,14 @@ class App extends Component {
           placeholder="search monsters"
           onChange={onSearchChange}
         />
-        {filteredMonsters.map((monster) => {
+        {/* {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>;
             </div>
           );
-        })}
+        })} */}
+        <CardList />
       </div>
     );
   }
